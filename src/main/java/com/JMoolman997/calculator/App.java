c5db95775bfcc6a1ce68285b284d25e7d1103ac6
Johannes Willemse Moolman <26892154@sun.ac.za>
2024-06-12 11:29:14 +0200

    Initial commit

---
+++ src/main/java/com/JMoolman997/calculator/App.java
@@ -0,0 +1,13 @@
+package com.JMoolman997.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
